3059b8460e57d57375b21b9b9f7e330f0ff60250
Pankaj Sahu <81400644+pankajsao11@users.noreply.github.com>
2024-11-01 17:13:02 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 #tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket-999"
+  bucket = "my-tf-bucket-action999"
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
